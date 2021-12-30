39f09b9403c6c4b54766ae3d7ac47f28aa0912e4
hongfei_ren <hongfei_ren@intsig.net>
2021-12-30 15:34:40 +0800

    [WN] [1.0.0] 基础组件 - 数据库lib 在主项目中增加测试代码 验证可用性

--- LuciusTower_Android/app/src/main/java/com/lucius/luciustower/MainActivity.kt
+++ LuciusTower_Android/app/src/main/java/com/lucius/luciustower/MainActivity.kt
@@ -2,10 +2,47 @@ package com.lucius.luciustower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.intsig.roomdb.entity.Person
+import com.intsig.roomdb.util.LuciusTowerDatabaseUtil
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        findViewById<View?>(R.id.tv_insert)?.setOnClickListener {
+            insertPerson()
+        }
+        findViewById<View?>(R.id.tv_check)?.setOnClickListener {
+            checkPersonSex()
+        }
+    }
+
+    // todo Lucius 删除debug代码
+    private fun insertPerson() {
+        Log.d(TAG, "insertPerson")
+        try {
+            LuciusTowerDatabaseUtil.db?.personDao()?.insertAll(Person(123456L, "LuciusRen", "lucius", 1, null, Date()))
+        } catch (e: Throwable) {
+            Log.e(TAG, "insertPerson get error: $e")
+        }
+    }
+
+    private fun checkPersonSex() {
+        val person = LuciusTowerDatabaseUtil.db?.personDao()?.findByName("LuciusRen")?.getOrNull(0)
+        val sex = person?.sex
+        Log.d(TAG, "person = $person, sex = $sex")
+
+        val peopleList = LuciusTowerDatabaseUtil.db?.personDao()?.getAll()
+        Log.d(TAG, "personList = ${peopleList?.size},")
     }
 }
