77828cbfdb8d84f1fca786f49a1dfebfa173fdfa
hongfei_ren <hongfei_ren@intsig.net>
2022-01-24 16:16:23 +0800

    [RC] [1.0.0] 移除BRVAH的gradle依赖

--- LuciusTower_Android/app/src/main/java/com/lucius/luciustower/MainActivity.kt
+++ LuciusTower_Android/app/src/main/java/com/lucius/luciustower/MainActivity.kt
@@ -2,14 +2,9 @@ package com.lucius.luciustower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.fragment.app.Fragment
 import com.blankj.utilcode.util.LogUtils
-import com.intsig.roomdb.entity.Person
-import com.intsig.roomdb.util.LuciusTowerDatabaseUtil
 import com.lucius.luciustower.person.PersonListFragment
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -35,22 +30,4 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(containerId, fragment)
         fragmentTransaction.commitAllowingStateLoss()
     }
-
-//    private fun insertPerson() {
-//        Log.d(TAG, "insertPerson")
-//        try {
-//            LuciusTowerDatabaseUtil.db?.personDao()?.insertAll(Person(123456L, "LuciusRen", "lucius", 1, null, Date()))
-//        } catch (e: Throwable) {
-//            Log.e(TAG, "insertPerson get error: $e")
-//        }
-//    }
-//
-//    private fun checkPersonSex() {
-//        val person = LuciusTowerDatabaseUtil.db?.personDao()?.findByName("LuciusRen")?.getOrNull(0)
-//        val sex = person?.sex
-//        Log.d(TAG, "person = $person, sex = $sex")
-//
-//        val peopleList = LuciusTowerDatabaseUtil.db?.personDao()?.getAll()
-//        Log.d(TAG, "personList = ${peopleList?.size},")
-//    }
 }
